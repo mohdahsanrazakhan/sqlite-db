7d25b1e8d9c4c8606ccafab0f3c049c1ffe80c77
Mark <datawithmark@gmail.com>
2024-08-13 23:51:01 +0530

    fix: delete router of serviceRoutes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -6,6 +6,6 @@ router.post('/services', serviceController.createService);
 router.get('/services', serviceController.getAllService);
 router.get('/services/:id', serviceController.getServiceById);
 router.put('/services/:id', serviceController.updateService);
-router.put('/services/:id', serviceController.deleteService);
+router.delete('/services/:id', serviceController.deleteService);
 
 module.exports = router;
